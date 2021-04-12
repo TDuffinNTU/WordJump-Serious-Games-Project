c4b9605fcaea2c23db5579d03e95c877896ff178
Thomas Duffin <43959652+TDuffinNTU@users.noreply.github.com>
2021-04-12 22:55:37 +0100

    Added Time Slider
    Planning to make coin collection necessary to boost remaining time.

--- WordJumper/Assets/Scripts/GameController.cs
+++ WordJumper/Assets/Scripts/GameController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public  class GameController : MonoBehaviour
 {
@@ -8,14 +9,19 @@ public  class GameController : MonoBehaviour
     public LevelGenerator _Gen;
     public ScoreController _Scr;
     public CameraController _Cmr;
+    public Slider _TimeSlider;
 
     //[SerializeField]
     Canvas _GameUI, _PausedUI, _GameOverUI, _PregameUI, _ScoreUI;
+    
     
 
     [SerializeField]
     private string State = "UNPAUSED";
 
+    private const float MAXTIME = 20f;
+    private const float COINTIME = 3f;
+    private float TimeLeft = MAXTIME;
     
     private float killY;
 
@@ -27,6 +33,7 @@ public  class GameController : MonoBehaviour
         _GameOverUI = GameObject.Find("GameOverUI").GetComponent<Canvas>();
         _PregameUI = GameObject.Find("PregameUI").GetComponent<Canvas>();
         _ScoreUI = GameObject.Find("ScoreUI").GetComponent<Canvas>();
+        
 
         SetGamestate("PREGAME");
     }
@@ -34,6 +41,20 @@ public  class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        // timer decrements while playing
+        if (State == "UNPAUSED")
+        {
+            TimeLeft -= Time.deltaTime;
+            _TimeSlider.value = TimeLeft / MAXTIME;
+
+            // Gameover when timer below 0
+            if (TimeLeft <= 0) 
+            {
+                SetGamestate("PAUSED");
+            }
+        }
+
         // player loses if they fall off the bottom of screen, calculated here
         killY = CameraExtensions.GetEdges(Camera.main)[3];
         if (_Plr.transform.position.y < killY) 
@@ -41,10 +62,11 @@ public  class GameController : MonoBehaviour
             SetGamestate("PAUSED");
         }
 
+        // starting the game
         if (Input.GetKeyDown(KeyCode.Space) && State == "PREGAME") 
         {            
             _ScoreUI.enabled = true;
-            _PregameUI.enabled = false;
+            _PregameUI.enabled = false;            
             SetGamestate("UNPAUSED");
         }
         
@@ -101,8 +123,12 @@ public  class GameController : MonoBehaviour
     }
 
 
+    public void AddTime() { TimeLeft += COINTIME; }
+
+
     public void Replay() // reset gameplay field (doesn't require scene reload)
     {
+        TimeLeft = MAXTIME;
         _Plr.Replay();
         _Gen.Replay();
         _Scr.Replay();
